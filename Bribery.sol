766f13ef48edf1a49ba0b271a35831da1bc6b84c
Laser420 <38286334+Laser420@users.noreply.github.com>
2022-06-12 01:27:30 -0400

    Update Bribery.sol

--- Bribery.sol
+++ Bribery.sol
@@ -7,30 +7,29 @@ contract BalancerBriber {
     address tribeCouncil;
     address governor; 
 
-    uint256 lastCall; 
-
     IBalancerBriber BalancerBribeVault;
     IERC20 feiInterface;
     
     constructor() {
-        governor = 0x72b7448f470D07222Dbf038407cD69CC380683F3; // Someone who can vetoe any transactions during the timelock
-        tribeCouncil = 0x2EC598d8e3DF35E5D6F13AE2f05a7bB2704e92Ea; //set this address to the tribe council multi-cig address
-        BalancerBribeVault = IBalancerBriber(0x7Cdf753b45AB0729bcFe33DC12401E55d28308A9); // an StETH address I found on rinkeby - speedread the code and its pretty much golden
-        feiInterface = IERC20(0x956F47F50A910163D8BF957Cf5846D573E7f87CA);
-        lastCall = block.timestamp;
+        governor = 0x72b7448f470D07222Dbf038407cD69CC380683F3; // Chosen individual capable of calling transactions without any timelock
+        tribeCouncil = 0x2EC598d8e3DF35E5D6F13AE2f05a7bB2704e92Ea; //tribe-council multi-sig address
+        BalancerBribeVault = IBalancerBriber(0x7Cdf753b45AB0729bcFe33DC12401E55d28308A9); // Instantiate (Its 1:00am and I forgot if the word instantiate applies here) the interface
+        feiInterface = IERC20(0x956F47F50A910163D8BF957Cf5846D573E7f87CA); //instantiate the interface (Im using it now. Burn my pants)
     }
 
+    //Verify the caller is the tribeCouncil address
     modifier isTribeCouncil() {
         require(msg.sender == tribeCouncil, "You aren't the tribe council...");
         _;
     }
-
+    
+    //Verify the caller is the CHOSEN ELECT LORD GENERAL SUPREMEME COMMANDER GOVERNOR SENATOR IVE HAD A STROKE
     modifier isGovernor() {
         require(msg.sender == governor, "You aren't the governor...");
         _;
     }
 
-    //Withdraw any FEI in this contract back to the Tribe Council Address
+    //Withdraw any FEI in this contract directly to the Tribe Council Address - nowhere else
     function withdrawFEI() public isTribeCouncil {
         uint256 bal = feiInterface.balanceOf(address(this));
         feiInterface.approve(address(this), bal);
@@ -38,21 +37,17 @@ contract BalancerBriber {
     }
 
 
-
     function executeBribeGovernor() external isGovernor {
-
+        //run functionality 
     }
 
    function executeBribeCouncil() external isTribeCouncil {
        //run functionality 
    }
-
-
-   
-
 }
 
-
+//HiddenHand BalancerBriber interface
+// https://etherscan.io/address/0x7Cdf753b45AB0729bcFe33DC12401E55d28308A9#code
 interface IBalancerBriber {
 
      function depositBribeERC20(
@@ -85,3 +80,6 @@ interface IERC20 {
         uint256 amount
     ) external returns (bool);
 }
+
+
+
